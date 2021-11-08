f412ab01f3966d27547f0c3d985d5848612e90b4
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2021-11-07 19:08:49 -0600

    add created date to mutterings

--- muttering.js
+++ muttering.js
@@ -1,7 +1,8 @@
 module.exports = class Muttering {
-    constructor(id, user, content) {
+    constructor(id, user, content, create_date) {
         this.id = id;
         this.user = user;
         this.content = content;
+        this.create_date = (typeof create_date === 'undefined') ? Date.now() : create_date;
     }
 }
