82ae2994d388d479960197e8354dfa27553a2cb4
Mike Beasterfeld <mike.beasterfeld@gmail.com>
2021-11-07 19:55:22 -0600

    add time_since_created method to Muttering class

--- muttering.js
+++ muttering.js
@@ -5,4 +5,18 @@ module.exports = class Muttering {
         this.content = content;
         this.create_date = (typeof create_date === 'undefined') ? new Date() : create_date;
     }
+
+
+
+    
+    time_since_created(current_time) {
+        current_time = (typeof current_time === 'undefined') ? new Date() : current_time;
+
+        console.log(typeof current_time);
+
+        // TODO: find super cool library later
+        let diff = current_time.getTime() - this.create_date.getTime();
+
+        return '(' + Math.floor(diff / 60000) + ' minute ago)';
+    }
 }
